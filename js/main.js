bf27bda1378eed91adc419ed9d639c90ec314a7c
kcy230426 <132032089+kcy230426@users.noreply.github.com>
2023-06-20 10:57:25 +0900

    첫번째 커밋
    첫번째 커밋

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper("#section .swiper", {
+    spaceBetween: 30,
+    effect: "fade",
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
